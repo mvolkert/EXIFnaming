73d91d47234452148863e0dca705fe7b21547a4d
Marco Volkert <marco.volkert24@gmx.de>
2017-12-27 15:14:50 +0100

    added comments to start

--- start.py
+++ start.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
-# exec(open("./start.py").read())
+
+#when using anaconda, copy this file to: C:\Users\[Name]\.ipython\profile_default\startup
+
+#start ipython
 exec (open("D:\ProgramData\Anaconda3\Scripts\ipython-script.py").read())
+
+#imports for utilities often used in ipython
 import time
 import os
 from os import listdir, rename, walk
@@ -10,12 +15,18 @@ from collections import OrderedDict
 from shutil import copyfile
 from send2trash import send2trash
 
-# for reloading
+# for reloading of scripts
 from IPython import get_ipython
-
 ipython = get_ipython()
 ipython.magic('load_ext autoreload')
 ipython.magic('autoreload 2')
 # for external reloading:
 # %load_ext autoreload
 # %autoreload 2
+
+#change to working dir
+print(os.getcwd())
+os.chdir("F:\\Bilder\\bearbeitung\\tags\\pyScripts")
+print(os.getcwd())
+
+import tags
--- start_pic.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-exec (open("./start.py").read())
-
-print(os.getcwd())
-os.chdir("E:\\Bilder\\bearbeitung\\tags")
-print(os.getcwd())
-
-import tags
