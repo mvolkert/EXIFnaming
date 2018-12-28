279125c3d41a162f9fcc663a8334cc62687e7832
Marco Volkert <marco.volkert24@gmx.de>
2018-12-28 22:46:38 +0100

    improve steps

--- EXIFnaming/steps.py
+++ EXIFnaming/steps.py
@@ -3,17 +3,13 @@
 Steps combine multiple functions
 """
 
-import os
-
-from EXIFnaming.nameop import filter_series, rename_HDR
+from EXIFnaming.nameop import filter_series, rename_HDR, sanitize_filename
 from EXIFnaming.readexif import order, rename, rotate
-from EXIFnaming.setexif import name_to_exif, geotag
+from EXIFnaming.setexif import geotag, read_csv
 
 
 def step1_prepare():
     order()
-    os.mkdir('.gps')
-    os.mkdir('.info')
 
 
 def step2_rename(Prefix="", dateformat='YYMMDD', startindex=1, onlyprint=False, postfix_stay=True, name=""):
@@ -25,14 +21,27 @@ def step2_rename(Prefix="", dateformat='YYMMDD', startindex=1, onlyprint=False, 
 
 
 def step3_filter():
+    """
+    filter Series
+    """
     filter_series()
 
 
-def step4_sanitzeHDR(subname="HDRT", folder="HDR"):
-    rotate(subname=subname, folder=folder, sign=1, override=True)
+def step4_sanitize(subname="HDRT", folder="HDR"):
+    """
+    sanitize postprocessing
+    :param subname: name of HDR
+    :param folder: name of HDR folder
+    :return:
+    """
     rename_HDR(mode=subname, folder=folder)
+    rotate(subname=subname, folder=folder, sign=1, override=True)
+    sanitize_filename()
 
 
-def step5_write_exif():
-    name_to_exif(additional_tags=(), startdir=None)
+def step5_write_exif(csv_restriction="fav"):
+    """
+    write exif via csv, filename and gpx files
+    """
+    read_csv("*", csv_restriction=csv_restriction)
     geotag(timezone=2, offset="")
