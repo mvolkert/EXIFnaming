b04dcb0ab30f89c6e55d42acdec4d9c613000480
Marco Volkert <marco.volkert24@gmx.de>
2019-10-25 18:22:11 +0200

    improve setup

--- setup.py
+++ setup.py
@@ -1,22 +1,39 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+with open("requirements.txt", "r") as file:
+    requirements = file.readlines()
 
 setuptools.setup(
     name="EXIFnaming",
     version="PYPIVERSION",
     author="Marco Volkert",
     author_email="marco.volkert24@gmx.de",
-    description="collection of Tag operations",
+    description="Tools for organizing and tagging photos",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mvolkert/EXIFnaming",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Multimedia :: Graphics :: Capture :: Digital Camera",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Other Audience",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Natural Language :: English",
+        "Natural Language :: German",
+        "Typing :: Typed",
     ],
+    keywords='exif exiftool photo tagging geotagging renaming photo-mode ordering python ' +
+             'lumix panasonic camera digital-camera photos camera-model TZ101 organizing',
     python_requires='>=3.6',
+    dependency_links=['https://sno.phy.queensu.ca/~phil/exiftool/'],
+    install_requires=requirements,
 )
