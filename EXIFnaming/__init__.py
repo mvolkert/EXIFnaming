124d1a78b52dced262030288028f49600e1a9536
Marco Volkert <marco.volkert24@gmx.de>
2019-10-26 19:01:14 +0200

    make settings accessible from main module

--- EXIFnaming/__init__.py
+++ EXIFnaming/__init__.py
@@ -14,10 +14,12 @@ __status__ = "Development"
 __all__ = ["readexif", "setexif", "nameop", "picture", "steps", "placeinfo"]
 
 from EXIFnaming import readexif, setexif, nameop, picture, steps, placeinfo
-from EXIFnaming.readexif import print_info, rename_pm, rename, order, searchby_exiftag_equality, \
-    searchby_exiftag_interval, rotate, exif_to_name, print_timetable, better_gpx_via_timetable
+from EXIFnaming.helpers import settings
+from EXIFnaming.helpers.decode import read_exiftags, write_exiftags
 from EXIFnaming.nameop import filter_series, rename_back, filter_primary, rename_HDR, rename_temp_back, folders_to_main, \
     copy_subdirectories
 from EXIFnaming.picture import detectBlurry, detectSimilar
+from EXIFnaming.readexif import print_info, rename_pm, rename, order, searchby_exiftag_equality, \
+    searchby_exiftag_interval, rotate, exif_to_name, print_timetable, better_gpx_via_timetable
 from EXIFnaming.setexif import shift_time, add_location, geotag, name_to_exif, fake_date, read_csv
 from EXIFnaming.steps import step1_prepare, step2_rename, step3_filter, step4_sanitize, step5_write_exif, make_fav
