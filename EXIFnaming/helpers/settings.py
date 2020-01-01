6f8005e96c5c5772f1aa66c8731fbf2c3b1ad6a4
Marco Volkert <marco.volkert24@gmx.de>
2020-01-01 19:08:15 +0100

    improve settings

--- EXIFnaming/helpers/settings.py
+++ EXIFnaming/helpers/settings.py
@@ -1,14 +1,21 @@
+#!/usr/bin/env python3
+"""
+Settings - modfy as you like
+
+encoding_format: the encoding_format may change on different os - tested on windows
+exiftool_directory: put here path/to/exiftool.exe (download: https://sno.phy.queensu.ca/~phil/exiftool/) if unset is same directory as settings
+googlemaps_api_key: if you want to use placeinfo.py - you need a api key of google
+loglevel: preset is DEBUG(10) if you want printed less - set it to INFO(20)
+photographer: Here comes your name
+"""
 includeSubdirs = True
-# the encoding_format may change on different os - tested on windows
 encoding_format = "latin"
 image_types = (".JPG", ".jpg", ".tif", ".tiff", ".hdr", ".exr", ".ufo", ".fpx", ".RW2", ".Raw")
 video_types = (".MP4", ".mp4")
 hdr_program = "franzis HDR projects"
 panorama_program = "Hugin"
-photographer = "Marco Volkert"
-standard_kamera = "DMC-TZ101"
-# if you want to use placeinfo.py - you need a api key of google
+photographer = ""
+standard_kamera = ""
 googlemaps_api_key = ""
-# if unset is same directory as settings
 exiftool_directory = r""
 loglevel=10
